69a6a315940d0c810514358eccf9ed1c1c87b9d6
JakeyRoundHead <56478950+stevesgenes@users.noreply.github.com>
2026-09-24 18:40:34 +0200

    Mockup

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return(
+        <div align="center">
+            <header>
+                <h1>Pomodoro Timer</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
