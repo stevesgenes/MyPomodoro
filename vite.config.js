d009af6b851eb9e77ab8f8a2427c0f4b69f44256
stevesgenes <56478950+stevesgenes@users.noreply.github.com>
2026-09-26 09:08:12 +0200

    Update package.json and package-lock.json to include vite-plugin-singlefile; modify vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
+
+// change base path to '/MyPomodoro/' for GitHub Pages deployment
+// change base path to './' for local development/npm builds
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/MyPomodoro/',
+  plugins: [react(), viteSingleFile()],
+  base: './',
 })
